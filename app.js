228d24472e681d2cbd4b77b47b7bf974294998ef
Ezgi Erel <ezgierel98@gmail.com>
2023-04-11 01:38:19 +0300

    keyboard added

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-export default function Square() {
-    return <button className="square">X</button>;
-}
+const keyboard = document.getElementById("keyboard-container");
+
+const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '⌫'];
+
+keys.forEach(key => {
+    const keyboardButton = document.createElement('button');
+    keyboardButton.textContent = key;
+    keyboardButton.value = key;
+    keyboard.append(keyboardButton);
+})
